cfd1a632eab65b827fa78952e8f4710089be2473
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 21:11:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,16 +1,20 @@
 # Add your variables here
 first_number = 3
 second_number = 6
+sum = "+"
+difference = "-"
+product = "*"
+quotient = "/"
 
 def calculate(first_number, second_number, operator)
   case operator
-    when "+"
+    when sum
       puts (first_number + second_number)
-    when "-"
+    when difference
       puts (first_number - second_number)
-    when "*"
+    when product
       puts (first_number * second_number)
-    when "/"
+    when quotient
       puts (first_number / second_number)
   end
 end
