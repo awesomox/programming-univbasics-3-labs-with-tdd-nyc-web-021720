daea9d7a0b365dfc6e79f3d7bb9f6e7a9ae37fd0
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 21:02:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 def calculator(first_number, second_number, operator)
-  first_number.to_f 
-  second_number.to_f 
+  first_number.to_i 
+  second_number.to_i 
   case operator
     when "+"
       puts (first_number + second_number)
