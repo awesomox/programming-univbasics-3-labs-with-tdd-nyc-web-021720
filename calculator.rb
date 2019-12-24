b5ad1a0174a0fb7bc3f2609acab6954e92dfb5e1
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 21:06:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,10 @@
 # Add your variables here
-def calculator(first_number = 0, second_number = 0, operator)
-  first_number.to_i 
-  second_number.to_i 
+first_number = 3
+second_number = 6
+
+def calculate(first_number, second_number, operator)
   case operator
     when "+"
-      puts (first_number + second_number)
-    when "-"
-      puts (first_number - second_number)
-    when "*"
-      puts (first_number * second_number)
-    when "/"  
-      puts (first_number / second_number)
-  end
-end
-
-calculator(8, 9, "*")
+      sum(first_number, )
 
     
